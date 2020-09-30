5318c401d1c5e3ebdb0f50489a5de1860a44b30c
shibudaniel <47894492+shibuadaniel@users.noreply.github.com>
2020-09-30 16:06:21 -0400

    MapView; Image; This completes the view

--- JWBranches/ContentView.swift
+++ JWBranches/ContentView.swift
@@ -10,19 +10,31 @@ import CoreData
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("US Branch")
-                .font(.title)
-                .foregroundColor(.blue)
-            HStack {
-                Text("PATTERSON")
-                    .font(.subheadline)
-                Spacer()
-                Text("New York, NY")
-                    .font(.subheadline)
+        VStack {
+            MapView()
+                .edgesIgnoringSafeArea(.top)
+                .frame(height: 300)
+            
+            USImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("US Branch")
+                    .font(.title)
+                    .foregroundColor(.blue)
+                HStack {
+                    Text("PATTERSON")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("New York, NY")
+                        .font(.subheadline)
+                }
             }
+            .padding()
+            
+            Spacer()
         }
-        .padding()
     }
 }
     
--- JWBranches/USImage.swift
+++ JWBranches/USImage.swift
@@ -22,5 +22,6 @@ struct USImage: View {
 struct USImsage_Previews: PreviewProvider {
     static var previews: some View {
         USImage()
+            
     }
 }
